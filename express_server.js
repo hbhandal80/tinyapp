76a955dd081b24bea5781ca07402087acf42db3f
Hervinder <hbhandal80@hotmail.com>
2021-05-14 08:10:23 +0000

    bcrypted the password

--- express_server.js
+++ express_server.js
@@ -2,7 +2,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080; 
+const bcrypt = require('bcrypt');
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,12 +26,12 @@ const users = {
   "userRandomID": {
     id: "userRandomID", 
     email: "user@example.com", 
-    password: "1234"
+    password: "$2b$10$rvHo2hYewT/xCwPsuA4/5.difT3mwhJLc42BgjnqxIwfxs6fXJpoe" //password '1234'
   },
  "user2RandomID": {
     id: "user2RandomID", 
     email: "user2@example.com", 
-    password: "hello"
+    password: "$2b$10$ocTTDUb4Pn17fNRpEbSAC.J2RywHA1AA9bTSSfrZJ/60ww1WeHmNq" //password 'hello'
   }
 };
 
@@ -137,11 +138,11 @@ app.post("/login", (req, res) => {
     res.status(400).send("Please enter a valid email and password");
   } else if (!userExists(email)) {
     res.status(403).send("This email is not registered");
-  } else if (userIDEmail(email)["password"] !== password) {  
-    res.status(403).send("Incorrect password");
+  } else if (bcrypt.compareSync(password, userIDEmail(email)["password"])) {  
+    res.cookie("userID", (userIDEmail(email)["id"]));
+    res.redirect("/urls");   
   } else {
-  res.cookie("userID", (userIDEmail(email)["id"]));
-  res.redirect("/urls");  
+    res.status(403).send("Incorrect password");
 }
 });
 
@@ -160,7 +161,7 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
   const email = req.body.email;
-  const password = req.body.password;
+  const password = bcrypt.hashSync(req.body.password, 10);
 
   if (!email || !password) {
     res.status(400).send("Please enter a valid email and password");
@@ -170,8 +171,8 @@ app.post("/register", (req, res) => {
   const userID = generateRandomString();
   users[userID] = {
     id: userID,
-    email: req.body.email, 
-    password: req.body.password,
+    email: email, 
+    password: password,
   };
   console.log(users);
   res.cookie("userID", userID);
